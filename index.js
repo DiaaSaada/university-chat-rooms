f20941ccf4700573b3f4a5fe33efc1712f63f77a
Diaa <dia.saada@gmail.com>
2024-03-30 22:16:08 +0400

    using templating with ejs & make rooms dynamic

--- index.js
+++ index.js
@@ -2,14 +2,25 @@ const app = require('express')();
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
 const port = 3003
-
+const path = require('path');
 
 server.listen(port, () => {
     console.log(`Server is running on ${port}`);
 });
 
+app.set('view engine', 'ejs');
+
+// Set the views directory
+app.set('views', path.join(__dirname, 'views'));
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html')
+});
+
+
+app.get('/:roomId', (req, res) => {
+    const roomId = req.params.roomId;
+    // Render the index.ejs file with the roomId value
+    res.render('index', {roomId: roomId});
 });
 
 // tech namespace
@@ -19,14 +30,15 @@ tech.on('connection', (socket) => {
     console.log('user connected');
     socket.on('msg', (data) => {
         console.log(`message: ${data.room}`);
-        tech.emit('msg', data.msg);
+        tech.in(data.room).emit("msg", data.msg);
+        // samae as
+        // tech.to(data.room).emit("msg", data.msg);
     });
 
     socket.on('join', (data) => {
         console.log(`request to join: ${data.room}`);
         socket.join(data.room);
         console.log(`joined: ${data.room}`);
-        tech.in(data.room).emit("msg", `New User joined room ${data.room}`);
         tech.to(data.room).emit("msg", `New User joined room ${data.room}`);
     });
 
